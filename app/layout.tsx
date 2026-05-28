a0deb1753a2e5931eb5c191abda99db435fbb3aa
VANSHI <vanshi@TryCatchs-MacBook-Air-2.local>
2026-05-28 15:26:48 +0530

    added marker io feedback system

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,21 +35,53 @@ export default function RootLayout({
       >
         {children}
 
-        {/* Marker.io Config */}
-        <Script id="marker-config">
+        {/* Marker.io Integration */}
+        <Script id="marker-io" strategy="afterInteractive">
           {`
-            window.MarkerConfig = {
-              // destination: "POR-1"
-             project: '6a18089b15ec6c6cf18dd125', 
+            window.markerConfig = {
+              project: '6a18089b15ec6c6cf18dd125',
+              source: 'snippet'
             };
+
+            !function(e,r,a){
+              if(!e.__Marker){
+                e.__Marker={};
+
+                var t=[],n={__cs:t};
+
+                [
+                  "show",
+                  "hide",
+                  "isVisible",
+                  "capture",
+                  "cancelCapture",
+                  "unload",
+                  "reload",
+                  "isExtensionInstalled",
+                  "setReporter",
+                  "clearReporter",
+                  "setCustomData",
+                  "on",
+                  "off"
+                ].forEach(function(e){
+                  n[e]=function(){
+                    var r=Array.prototype.slice.call(arguments);
+                    r.unshift(e),t.push(r)
+                  }
+                });
+
+                e.Marker=n;
+
+                var s=r.createElement("script");
+                s.async=1;
+                s.src="https://edge.marker.io/latest/shim.js";
+
+                var i=r.getElementsByTagName("script")[0];
+                i.parentNode.insertBefore(s,i)
+              }
+            }(window,document);
           `}
         </Script>
-
-        {/* Marker.io Script */}
-        <Script
-          src="https://edge.marker.io/latest/shim.js"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
